a18e7aafa450920d1e3481fc71491d99af12e672
Aniket <aniket.bst454@gmail.com>
2025-10-08 17:23:18 +0530

    added all the functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,37 @@ function App() {
         /> 
         <button className='outline-none text-white bg-blue-700 px-3 py-0.5 shrink-0'>copy</button>
       </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type='range'
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setlength(e.target.value)}}
+          />
+            <label>Length:{length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type='checkbox'
+          defaultChecked={numberAllowed}
+          id='numberInput'
+          onChange={()=>{setnumberAllowed((prev)=>!prev)}}
+          />
+          <label>Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type='checkbox'
+          defaultChecked={charAllowed}
+          id='charInput'
+          onChange={()=>{setcharAllowed((prev)=>!prev)}}
+          />
+          <label>Character</label>
+        </div>
+      </div>
     </div>
     </>
   )
